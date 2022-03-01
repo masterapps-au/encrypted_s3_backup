85d9b88ae2fb408986c49ac3000dd2b7caa51bd1
Ryan Butterfield <git@ryanbutterfield.com>
2022-03-01 13:44:44 +1100

    - Output the container timezone when starting.

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,7 +6,7 @@ echo "SHELL=/bin/bash
 BASH_ENV=/container.env
 $CRONTAB /encrypted_s3_backup.py >/proc/1/fd/1 2>/proc/1/fd/2
 " > scheduler.txt
-echo "encrypted_s3_backup docker has started with crontab of: $CRONTAB" >/proc/1/fd/1 2>/proc/1/fd/2
+echo "encrypted_s3_backup docker has started with crontab of: $CRONTAB ($TZ)" >/proc/1/fd/1 2>/proc/1/fd/2
 set +o noglob
 crontab scheduler.txt
 cron -f
